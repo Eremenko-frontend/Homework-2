9d1388753ebb6f6dae7a6e296c0a236d38c2a5c4
Eremenko-frontend <AleksantdrEremenko1988@gmail.com>
2018-11-02 02:13:04 +0200

    bug fix

--- js/main.js
+++ js/main.js
@@ -1,70 +1,85 @@
-var massiv1 = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
-var massiv2 = [-1,-8,-2];
-var massiv3 = [1,7,3];
-var massiv4 = [1,undefined,3,5,-3];
-var massiv5 = [1,NaN,3,5,-3];
+var massiv1 = [3, 0, -5, 1, 44, -12, 3, 0, 0, 1, 2, -3, -3, 2, 1, 4, -2-3-1];
+var massiv2 = [-1, -8, -2];
+var massiv3 = [1, 7, 3];
+var massiv4 = [1, undefined, 3, 5, -3];
+var massiv5 = [1, NaN, 3, 5, -3];
 
 function CustomMathOper (MathematicalOperation, massiv) {
 
+  //creating a new array for action on it
 
-    //creating a new array for action on it
+  var newCustomMasive = [];
+	
+  for (var x = 0; massiv.length > x; x++) {
 
-    var newCustomMasive = [];
-    for (var x = 0; massiv.length > x; x++) {
+    newCustomMasive[x] = massiv[x]
+  }
 
-        newCustomMasive[x] = massiv[x]
+
+  if (MathematicalOperation == 'MaxNumber') {
+
+    for (var i = 0; newCustomMasive.length > i; i++) {
+		
+      if(newCustomMasive[i] > newCustomMasive[0]){
+		  
+        newCustomMasive[0] = newCustomMasive[i];
+		  
+      }
+
     }
-
-
-    if (MathematicalOperation == 'MaxNumber') {
-
-
-        for (var i = 0; newCustomMasive.length > i; i++) {
-            if(newCustomMasive[i] > newCustomMasive[0]){
-                newCustomMasive[0] = newCustomMasive[i];
-            }
-
-        }
-        console.log(newCustomMasive[0]);
+	  
+    console.log(newCustomMasive[0]);
 
     } else if (MathematicalOperation == 'MinNumber') {
 
 
-        for (var i = 0; newCustomMasive.length > i; i++) {
-            if(newCustomMasive[i] < newCustomMasive[0]){
-                newCustomMasive[0] = newCustomMasive[i];
-            }
-
+      for (var i = 0; newCustomMasive.length > i; i++) {
+		  
+        if(newCustomMasive[i] < newCustomMasive[0]){
+			
+          newCustomMasive[0] = newCustomMasive[i];
         }
-        console.log(newCustomMasive[0]);
+
+      }
+      console.log(newCustomMasive[0]);
 
     } else if (MathematicalOperation == 'SumNumbers') {
 
 
-        var answerSum = 0;
-        for (var i = 0; newCustomMasive.length > i; i++) {
-            if (newCustomMasive[i] == undefined) {
-                var changeType = newCustomMasive[i];
-                newCustomMasive[i] = Boolean(changeType);
-                newCustomMasive[i] = Number(newCustomMasive[i]);
-            }
+    var answerSum = 0;
+		
+    for (var i = 0; newCustomMasive.length > i; i++) {
+		
+      if (newCustomMasive[i] == undefined) {
+		  
+        var changeType = newCustomMasive[i];
+		  
+        newCustomMasive[i] = Boolean(changeType);
+		  
+        newCustomMasive[i] = Number(newCustomMasive[i]);
+		  
+      }
 
-            if (isNaN(newCustomMasive[i])) {
+      if (isNaN(newCustomMasive[i])) {
 
-                newCustomMasive[i] = 0;
+        newCustomMasive[i] = 0;
 
-            }
-            answerSum = answerSum + newCustomMasive[i];
+      }
+		
+      answerSum = answerSum + newCustomMasive[i];
 
 
 
-        }
-        console.log(answerSum);
+    }
+		
+    console.log(answerSum);
 
     } else {
+		
         console.log('\'Зазначена Вами дія не передбачення інструкціями\'');
 
     }
+	
 }
 
 console.log("Перелік максимальних відповідей масивів:");
